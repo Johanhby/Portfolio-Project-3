bb29d4cf4488686e7c103c140ea503a94f3f3f41
Johanhby <johanhby@gmail.com>
2023-02-23 09:15:08 +0000

    add board and ship parameters

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Creating the game board.
+column = []
+row = []
+for y in range(8):
+    for x in range(8):
+        row.append('O')
+    column.append(row)
+
