c63d82a169c98784758e03586726b016c192d457
Johanhby <johanhby@gmail.com>
2023-02-24 11:29:31 +0000

    add class for data modeling purposes

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
-# Creating the game board.
+import random
+
+# Define game board.
+
 column = []
 row = []
-for y in range(8):
-    for x in range(8):
+for y in range(5):
+    for x in range(5):
         row.append('O')
     column.append(row)
 
+# Define amount of ships, grid parameters and list to contain ship placements.
+
+grid_width = 5
+grid_height = 5
+num_ships = 4
+ships = []
+
+#
